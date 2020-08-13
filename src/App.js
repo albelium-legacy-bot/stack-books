16f6b7c5fc2baa35d02310edc7e8ab0fba56ace0
takechee <takechee14@gmail.com>
2020-08-13 17:29:42 +0900

    ログインとプライバシーポリシーにアクセスできるようにする

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Privacy from "./pages/Privacy";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Top</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/privacy">PrivacyPolicy</Link></li>
+          </ul>
+
+          <Switch>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/privacy" component={Privacy}></Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
---
+++ src/pages/Privacy.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class Privacy extends React.Component {
+  render() {
+    return (
+      <h1>Privacy</h1>
+    );
+  }
+}
