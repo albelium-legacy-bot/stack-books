b512a26ef65fbd44b17b7a61e31d268ccc5297d3
shirai <shirai.ryohei0302@gmail.com>
2020-08-13 20:20:25 +0900

    アバウトページを追加

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Privacy from "./pages/Privacy";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <div>
           <ul>
             <li><Link to="/">Top</Link></li>
+            <li><Link to="/about">About</Link></li>
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/privacy">PrivacyPolicy</Link></li>
           </ul>
@@ -17,6 +19,7 @@ function App() {
           <Switch>
             <Route path="/login" component={Login}></Route>
             <Route path="/privacy" component={Privacy}></Route>
+            <Route path="/about" component={About}></Route>
           </Switch>
         </div>
       </Router>
