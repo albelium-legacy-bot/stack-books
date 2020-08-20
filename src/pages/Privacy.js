63cfb7acd13d5fcb1d79976f2203ac0c05cc747e
siropaca <ryhshr3@gmail.com>
2020-08-20 21:29:32 +0900

    ソース整理

--- src/pages/Privacy.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class Privacy extends React.Component {
-  render() {
-    return (
-      <h1>Privacy</h1>
-    );
-  }
-}
--- src/pages/top.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class Top extends React.Component {
-  render() {
-    return (
-      <h1>Top</h1>
-    );
-  }
-}
