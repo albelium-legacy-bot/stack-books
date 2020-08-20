b497e26bc3636f3e6459cc183a5ebaec6d5149ba
siropaca <ryhshr3@gmail.com>
2020-08-20 21:44:50 +0900

    LPを追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Top from "./pages/Top";
+import Lp from "./pages/Lp";
 import Login from "./pages/Login";
 import Privacy from "./pages/Privacy";
 import About from "./pages/About";
@@ -10,6 +11,7 @@ function App() {
   return (
     <Router>
       <Route exact path="/" component={Top}></Route>
+      <Route path="/lp" component={Lp}></Route>
       <Route path="/login" component={Login}></Route>
       <Route path="/privacy" component={Privacy}></Route>
       <Route path="/about" component={About}></Route>
